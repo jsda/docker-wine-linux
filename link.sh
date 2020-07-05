8c313a1d8fd597811239ec6824252be7942563cb
jasdada <12887817+jsda@users.noreply.github.com>
2020-07-05 13:20:02 +0800

    Update link.sh

--- link.sh
+++ link.sh
@@ -13,6 +13,10 @@ wget http://mirrors.aliyun.com/deepin/pool/non-free/d/deepin-wine/deepin-fonts-w
 wget http://mirrors.aliyun.com/deepin/pool/non-free/d/deepin-wine/deepin-libwine_2.18-22~rc0_i386.deb
 wget https://mirrors.aliyun.com/deepin/pool/main/libj/libjpeg-turbo/libjpeg62-turbo_1.5.1-2_amd64.deb
 wget https://mirrors.aliyun.com/deepin/pool/main/libj/libjpeg-turbo/libjpeg62-turbo_1.5.1-2_i386.deb
+#tools
+wget http://mirrors.aliyun.com/deepin/pool/non-free/d/deepin.com.qq.im/deepin.com.qq.im_9.1.8deepin0_i386.deb
+wget http://mirrors.aliyun.com/deepin/pool/non-free/d/deepin.com.wechat/deepin.com.wechat_2.6.8.65deepin0_i386.deb
+wget http://mirrors.aliyun.com/deepin/pool/non-free/d/deepin.com.qq.office/deepin.com.qq.office_2.0.0deepin4_i386.deb
 
 echo '准备添加32位支持'
 sudo dpkg --add-architecture i386
