c9690d800790d8d4ff0c3a2d3a31cf6ef8cb49e1
jasdada <12887817+jsda@users.noreply.github.com>
2021-02-20 18:52:52 +0800

    Update link.sh

--- link.sh
+++ link.sh
@@ -33,4 +33,4 @@ sudo apt install -fy
 
 rm -vfr /tmp/deepintemp
 ##make APP soft link
-#for i in $(ls /opt/deepinwine/apps/);do ln -s /opt/deepinwine/apps/$i/run.sh $(echo $i|awk 'BEGIN{FS="-"}{print "/usr/bin/"$2}');done
+for i in $(ls /opt/deepinwine/apps/);do ln -s /opt/deepinwine/apps/$i/run.sh $(echo $i|awk 'BEGIN{FS="-"}{print "/usr/bin/"$2}');done
